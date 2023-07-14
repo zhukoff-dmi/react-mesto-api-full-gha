539215d6ff7f64c358f034d3845836b89da8c8e3
zhukoff.dmi <happy-beard@mail.ru>
2023-07-14 23:26:48 +0300

    «PR15»

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const ERROR_NOT_FOUND = 404;
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb')
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => console.log('Подключено к MongoDB'))
   .catch((err) => {
     console.error('Ошибка подключения к MongoDB:', err);
