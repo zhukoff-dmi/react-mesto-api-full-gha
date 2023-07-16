337c8da2966349e851ef7610702017587fb6c06c
zhukoff.dmi <happy-beard@mail.ru>
2023-07-16 12:05:26 +0300

    Поменял домен

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.zhukoffdmi.student.nomoredomains.work";
 
 const getJson = (res) => {
     if (res.ok) {
