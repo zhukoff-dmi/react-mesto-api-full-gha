45476d277f5cfc6df06d89a9ee9afb1212b4ae0a
zhukoff.dmi <happy-beard@mail.ru>
2023-07-17 00:33:50 +0300

    test

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,13 +1,18 @@
 const jwt = require('jsonwebtoken');
-const AuthorizationError = require('../errors/AuthorizationError');
+// const AuthorizationError = require('../errors/AuthorizationError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
+
+const ANAUTHORUZED_ERROR = 401;
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthorizationError('Авторизация не пройдена'));
+    // return next(new AuthorizationError('Авторизация не пройдена'));
+    return res
+      .status(ANAUTHORUZED_ERROR)
+      .send({ message: 'Аторизируйтесь' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
@@ -15,7 +20,10 @@ module.exports.auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    throw new AuthorizationError('Авторизация не пройдена');
+    // throw new AuthorizationError('Авторизация не пройдена');
+    return res
+      .status(ANAUTHORUZED_ERROR)
+      .send({ message: 'Пользователь не авторизован' });
   }
   req.user = payload;
   return next();
