c5e830f6ebca5a1ea6d705663f89978419d45ca7
zhukoff.dmi <happy-beard@mail.ru>
2023-07-17 23:29:31 +0300

    Проектная работа 15.1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const userRouter = require('./users');
 const cardsRouter = require('./cards');
 const { auth } = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/users');
+const NotFoundError = require('../errors/NotFoundError');
 
 const validation = require('../middlewares/validation');
 
@@ -12,5 +13,8 @@ router.post('/signup', validation.createUser, createUser);
 
 router.use('/cards', auth, cardsRouter);
 router.use('/users', auth, userRouter);
+router.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
 
 module.exports = router;
